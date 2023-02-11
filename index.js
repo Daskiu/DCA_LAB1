a221a55aa3040fed635cba7b6d216cd2db43058f
Daniel Zapata <zapata0423@gmail.com>
2023-02-10 19:28:33 -0500

    V1.1

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log("Test");
+const isNumber = require('is-number');
+const math = require('mathjs');
+
+console.log(isNumber (5));
+
+console.log(isNumber (undefined));
+
+
+
+console.log(math.isBoolean(4));
